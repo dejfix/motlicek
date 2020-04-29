22d09dffb54fcd12bde7d4e7683a95599c1169a7
dejfix <60886619+dejfix@users.noreply.github.com>
2020-04-29 19:27:54 +0200

    Add files via upload

--- Motlicek_wd.py
+++ Motlicek_wd.py
@@ -1,53 +1,43 @@
 import argparse
 import sys
-
+import requests
 
 def main():
     muj_parser = argparse.ArgumentParser(description='Soubor.')
-    muj_parser.add_argument('soubor')
-    muj_parser.add_argument("radky", help="Spočítat řádky", action="store_true")
-    muj_parser.add_argument("znaky", help="Spočítat znaky", action="store_true")
-    muj_parser.add_argument("slova", help="Spočítat slova", action="store_true")
-
-    argumenty = muj_parser.parse_args()
+    muj_parser.add_argument("website", help="Název webu")
+    muj_parser.add_argument("--radky", help="Spočítat řádky", action="store_true")
+    muj_parser.add_argument("--znaky", help="Spočítat znaky", action="store_true")
+    muj_parser.add_argument("--slova", help="Spočítat slova", action="store_true")
+    args = muj_parser.parse_args()
 
     try:
-        f = open(argumenty.soubor, "r")
-        soubor = f.read()
-        flag = False
-        if argumenty.slova:
-            pocet_slov = word_counter(soubor)
-            print(f"Počet slov: {pocet_slov}")
-            flag = True
+        r = requests.get(args.website)
+        f = r.text
+        check = False
 
-        if argumenty.znaky:
-            pocet_znaku = znaky_counter(soubor)
-            print(f"Počet znaků: {pocet_znaku}")
-            flag = True
-
-        if argumenty.radky:
-            pocet_radku = line_counter(soubor)
+        if args.radky:
+            pocet_radku = len(f.split('\n'))
             print(f"Počet řádků: {pocet_radku}")
-            flag = True
+            check = True
 
-        if not flag:
-            pocet_slov = word_counter(soubor)
-            pocet_znaku = znaky_counter(soubor)
-            pocet_radku = line_counter(soubor)
-            print(f"Počet slov: {pocet_slov}\nPočet znaků: {pocet_znaku}\nPočet řádků: {pocet_radku}")
+        if args.znaky:
+            pocet_znaku = len(f)
+            print(f"Počet znaků: {pocet_znaku}")
+            check = True
+
+        if args.slova:
+            pocet_slov = f.count(' ') + len(f.split('\n'))
+            print(f"Počet slov: {pocet_slov}")
+            check = True
+
+        if check == False:
+            pocet_slov = f.count(' ') + len(f.split('\n'))
+            pocet_znaku = len(f)
+            pocet_radku = len(f.split('\n'))
+            print(f" Počet řádků: {pocet_radku}\n Počet znaků: {pocet_znaku}\n Počet slov: {pocet_slov}")
+
     except:
-        print("Houstne, máme problém. Něco se pokazilo. ")
+        print("Houstne, máme problém. Něco se pokazilo, asi chyba souboru.")
         sys.exit(1)
-
-def word_counter(f):
-    pocet_slov = f.count(' ') + len(f.split('\n'))
-    return pocet_slov
-    pass
-def line_counter(f):
-    pocet_radku = len(f.split('\n'))
-    return pocet_radku
-def znaky_counter(f):
-    pocet_znaku = len(f)
-    return pocet_znaku
 
 main()
